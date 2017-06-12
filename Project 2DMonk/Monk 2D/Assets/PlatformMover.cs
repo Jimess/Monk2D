5118b54167b78b2b3de7e3bf028968611a4ca6f5
LaurynasP <laurynas.pupsta@gmail.com>
2017-06-12 15:26:01 +0100

    Map and Interractibles
    * Created a map on Tiled and exported to Unity with colliders.
    
    * Map has been imported and collisions are working nice.
    
    * Interractible items are now working (Platforms and Switches)

---
+++ Project 2DMonk/Monk 2D/Assets/PlatformMover.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMover : MonoBehaviour {
+	//swtiches needed to activate for the platform to open
+	public SwitchController [] switches;
+	// the total width of the platform. current one square width is 1.
+	public int platformCount = 1;
+
+	// direction
+	public int direction = 1;
+
+
+	// Use this for initialization
+	void Start () {
+		foreach (SwitchController swt in switches) {
+			swt.SetMovingPart (this);
+		}
+	}
+	
+	public void OpenPlatform () {
+		bool canOpen = true;
+		foreach (SwitchController swt in switches) {
+			if (!swt.isActive) {
+				canOpen = false;
+				break;
+			}
+		}
+
+		//this part starts the animation of mover and deactivates collider when done.
+		if (canOpen) {
+			if (direction == 1) {
+				iTween.MoveBy (gameObject, iTween.Hash ("x", platformCount, "time", 2f, "easetype", iTween.EaseType.linear, "oncomplete", "FinishOpen"));
+			} else if (direction == -1) {
+				iTween.MoveBy (gameObject, iTween.Hash ("x", -platformCount, "time", 2f, "easetype", iTween.EaseType.linear, "oncomplete", "FinishOpen"));
+
+			}
+		}
+	}
+
+	public void FinishOpen () {
+		foreach (PolygonCollider2D obj in GetComponentsInChildren<PolygonCollider2D> ()) {
+			obj.enabled = false;
+		}
+	}
+}
---
+++ Project 2DMonk/Monk 2D/Assets/SwitchController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchController : MonoBehaviour {
+	public bool isActive = false;
+
+	private PlatformMover movingPart;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetMovingPart (PlatformMover go) {
+		movingPart = go;
+	}
+
+	public void OnTriggerStay2D (Collider2D col) {
+		if (col.transform.tag == "Player" && !isActive && Input.GetKey(KeyCode.E)) {
+			print (name + "Activated");
+			isActive = true;
+			movingPart.OpenPlatform ();
+		}
+	}
+}
